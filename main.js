9a04bc5bb053100c327d34b18f427f48d498719b
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2018-07-04 10:38:49 +0530

    Improved create board functionality - using a form instead of the wonky contentEditable attribute Now using localStorage to persist boards

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const boards = JSON.parse(localStorage.getItem('boards') || '[]');
 
 const main_node = document.querySelector('main');
 const create_board_html = `<div class="board create-board"><h1>Create Board...</h1></div>`;
-const new_board_html = `<div class="board new-board"><input type="text" name="new-board-name" placeholder="Enter name..." /></div>`
+const new_board_html = `<form id="new-board-form" class="board">
+                            <input type="text" name="board-name" placeholder="Enter name..." required autocomplete="off" />
+                        </form>`
 
 document.addEventListener('DOMContentLoaded', (e) => showBoards(boards));
 main_node.addEventListener('click', handleClick);
@@ -24,7 +26,7 @@ function renderBoards(boards) {
 function handleClick(e) {
     const target_class = e.target.className;
     if (target_class.includes('create-board') || e.target.parentNode.className.includes('create-board')) {
-        createBoard();
+        showNewBoardForm();
     } else if (target_class.includes('edit-board')) {
         // TODO: allow user to change board name
     } else if (target_class.includes('close-board')) {
@@ -34,23 +36,49 @@ function handleClick(e) {
     }
 }
 
-function createBoard() {
-    boards.unshift({ title: 'Board Name...', lists: [] });
-    showBoards(boards);
+function showNewBoardForm() {
+    if (main_node.querySelector('form#new-board-form') === null) {
+        main_node.innerHTML = new_board_html + main_node.innerHTML;
 
-    // TODO: can we do better?
-    document.querySelector('.board:first-child > h1').setAttribute('contentEditable', true);
+        const form = main_node.querySelector('form');
+        const input = form.querySelector('input[name="board-name"]');
+
+        form.addEventListener('click', e => input.focus());
+        form.addEventListener('submit', e => {
+            createBoard(input.value)
+            e.preventDefault();
+        });
+
+        // NOTE: clicking on "create board" when input is active leads to funny behaviour?
+        input.addEventListener('blur', e => {
+            if (e.explicitOriginalTarget !== form) hideNewBoardForm();
+        });
+        input.focus();
+    }
+}
+
+function hideNewBoardForm() {
+    const form = main_node.querySelector('form#new-board-form');
+    if (form !== null) main_node.removeChild(form);
+}
+
+function createBoard(name) {
+    boards.unshift({ title: name, lists: [] });
+    localStorage.boards = JSON.stringify(boards);
+    showBoards(boards);
 }
 
 function closeBoard(e) {
+    // get a reference to the board that has to be removed
     const to_close = e.target.parentNode.parentNode;
 
-    let idx = 0, cur = main_node.firstChild;
+    let idx = 0, cur = main_node.querySelector('.board');
     while (cur !== to_close) {
         cur = cur.nextSibling;
         idx++;
     }
 
-    main_node.removeChild(to_close);
     boards.splice(idx, 1);
+    localStorage.boards = JSON.stringify(boards);
+    main_node.removeChild(to_close);
 }
