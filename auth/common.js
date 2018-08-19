0aec03ea0862414579d37e6437b3e53f93ea20af
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2018-08-19 22:16:07 +0530

    Added a comment

--- auth/common.js
+++ auth/common.js
@@ -3,6 +3,9 @@ const jwt = require('jsonwebtoken');
 
 const config = require('../config');
 
+// we want to log the user in when:
+//   1. user logs in explicitly (with fb/google/password)
+//   2. user succesfully verified his/her email after registering with password
 function handleLoginSuccess(payload, res) {
     jwt.sign(payload, config.jsonwebtoken.key, { expiresIn: config.jsonwebtoken.expiry }, (err, token) => {
         if (err) {
