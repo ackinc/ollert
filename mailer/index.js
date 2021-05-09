1b22d6397cad546582370df50695de50426868ae
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2021-05-09 19:45:27 +0530

    No longer hardcoding from address of emails sent

--- mailer/index.js
+++ mailer/index.js
@@ -4,12 +4,12 @@ const pug = require("pug");
 const config = require("../config");
 const util = require("../libs/util");
 
-const mail_transporter = nodemailer.createTransport(config.email);
+const mail_transporter = nodemailer.createTransport(config.smtp);
 
 function sendEmail(type, to, data, cb = util.genericCallback) {
   const html = pug.renderFile(config.email_templates[type].template_path, data);
   const options = {
-    from: "anirudh.nimmagadda@gmail.com",
+    from: config.email_sending_address,
     to: to,
     subject: config.email_templates[type].subject,
     html: html,
